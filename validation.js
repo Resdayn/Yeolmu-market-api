07d85451212ef34de0081015a51b3638cceb4db0
Juan Gonzalez <jgonzalez@naver.com>
2022-03-23 17:02:36 +0900

    added validation for the items

--- validation.js
+++ validation.js
@@ -4,25 +4,37 @@ const Joi = require("@hapi/joi");
 
 // Register validation
 const registerValidation = (bodyRequest) => {
-    const schema = Joi.object({
-      username: Joi.string().min(6).required().max(30),
-      email: Joi.string().min(6).required().max(100).email(),
-      password: Joi.string().min(6).required().max(1024),
-      image: Joi.string()
-    });
-    return schema.validate(bodyRequest);
-  };
-  
-  // Login validation
-  const loginValidation = (bodyRequest) => {
-      const schema = Joi.object({
-        email: Joi.string().min(6).required().max(100).email(),
-        password: Joi.string().min(6).required().max(1024),
-      });
-      return schema.validate(bodyRequest);
-    };
-    
-  module.exports = {
-      registerValidation,
-      loginValidation
-  }
+  const schema = Joi.object({
+    username: Joi.string().min(6).required().max(30),
+    email: Joi.string().min(6).required().max(100).email(),
+    password: Joi.string().min(6).required().max(1024),
+    image: Joi.string(),
+  });
+  return schema.validate(bodyRequest);
+};
+
+// Login validation
+const loginValidation = (bodyRequest) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().max(100).email(),
+    password: Joi.string().min(6).required().max(1024),
+  });
+  return schema.validate(bodyRequest);
+};
+
+// Item Upload Validation
+const itemUploadValidation = (bodyRequest) => {
+  const schema = Joi.object({
+    title: Joi.string().required().min(1),
+    price: Joi.number().required(),
+    photo: Joi.string().required(),
+    details: Joi.string().required().min(1).max(600)
+  });
+  return schema.validate(bodyRequest);
+};
+
+module.exports = {
+  registerValidation,
+  loginValidation,
+  itemUploadValidation
+};
