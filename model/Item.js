0cbf25f8744085e0f5a32bd5f6b8c530ba848025
Juan Gonzalez <jgonzalez@naver.com>
2022-04-13 12:57:09 +0900

    added username

--- model/Item.js
+++ model/Item.js
@@ -1,28 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const itemSchema = mongoose.Schema({
-   title: {
-       type: String,
-       required: true,
-       min:1
-   },
-   price: {
-       type: Number,
-       required: true,
-   },
-   photo: {
-       type: String,
-       required: true
-   },
-   details: {
-       type: String,
-       required: true,
-       min: 10,
-       max: 600
-   }
+  username: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 30,
+  },
+  title: {
+    type: String,
+    required: true,
+    min: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  details: {
+    type: String,
+    required: true,
+    min: 10,
+    max: 600,
+  },
 });
 
-itemSchema.set('timestamps', true);
+itemSchema.set("timestamps", true);
 
-module.exports = mongoose.model('Item', itemSchema);
-
+module.exports = mongoose.model("Item", itemSchema);
