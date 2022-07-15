8ad486bf3935933ac6f7a5eb068c731b0daac25d
Juan Gonzalez <jgonzalez@naver.com>
2022-07-15 11:05:59 +0900

    added dummy data to mongodb after server initialization

--- server.js
+++ server.js
@@ -2,23 +2,38 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors = require('cors');
-const verifyToken = require('./routes/verifyToken');
+const Item = require("./model/Item"); // Import dummy data
+const cors = require("cors");
+const verifyToken = require("./routes/verifyToken");
 
 // Import Routes
 const authenticationRoute = require("./routes/authentication");
-const itemRoute = require('./routes/item');
+const itemRoute = require("./routes/item");
 
 // Initialize dotenv
 dotenv.config();
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URL)
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    authSource: "admin",
+    user: "admin",
+    pass: "password",
+  })
   .then((result) => {
     console.log("Connection to MongoDB successful");
     // Listening for requests if db connection successful
     app.listen(3000, () => console.log("Server Listening on port 3000..."));
+
+    // Importing dummy data to the db
+    const items = require("./mongodb-initdb/items");
+    Item.collection
+      .insertMany(items)
+      .then(() => console.log("Items Dummy Data inserted")) // Success)
+      .catch(function (error) {
+      });
   })
   .catch((error) => console.log(error));
 
@@ -31,7 +46,7 @@ app.use("/api/user", authenticationRoute);
 app.use("/api/item", itemRoute);
 
 // Protected TEST routes
-app.get('/api/protected', verifyToken, (request, response) => {
-  const userId = request.user._id
+app.get("/api/protected", verifyToken, (request, response) => {
+  const userId = request.user._id;
   response.send(`USER ${userId} CONFIRMED. PROTECTED ROUTE ACCESSED`);
 });
