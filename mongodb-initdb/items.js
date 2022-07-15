a98e2d81c3a36a281d1ec8826d037f2ddbee5d77
Juan Gonzalez <jgonzalez@naver.com>
2022-07-15 11:06:44 +0900

    added item dummy for mongodb

---
+++ mongodb-initdb/items.js
@@ -0,0 +1,74 @@
+const items = [
+  {
+    _id: "623ad204aa78f592f80755a2",
+    title: "Selling one dinning table (perfect condition)",
+    price: 40,
+    photo:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.cymaxstores.com%2FImages%2F89%2F1541987-L.jpg&f=1&nofb=1",
+    details:
+      "It's in perfect condition and very cheap. Contact me for details.",
+    createdAt: { $date: "2022-03-23T07:53:40.212Z" },
+    updatedAt: { $date: "2022-03-23T07:53:40.212Z" },
+    __v: 0,
+  },
+  {
+    _id: "623ad4eab003a2f12e5c7eea",
+    title: "Blue curtains",
+    price: 20,
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/yeolmu-api.appspot.com/o/items%2Fbluecurtains.jpeg?alt=media&token=43020948-901a-4795-b9ee-21f01be223c5",
+    details: "Can deliver anywhere",
+    createdAt: { $date: "2022-03-23T08:06:02.997Z" },
+    updatedAt: { $date: "2022-03-23T08:06:02.997Z" },
+    __v: 0,
+  },
+  {
+    _id: "623ad592627d27bbf7f4bded",
+    title: "Glass Collection",
+    price: 20,
+    photo:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-Jyozi2UfEmw%2FUwrTlCn2OuI%2FAAAAAAAAIMg%2FIFA64bRskPc%2Fs1600%2F007.JPG&f=1&nofb=1",
+    details: "All items intact.",
+    createdAt: { $date: "2022-03-23T08:08:50.958Z" },
+    updatedAt: { $date: "2022-03-23T08:08:50.958Z" },
+    __v: 0,
+  },
+  {
+    _id: "62564829c0c41f8186f7161c",
+    username: "Test_User",
+    title: "Silverware",
+    price: 400,
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/yeolmu-api.appspot.com/o/items%2FTest_User_item_1649821733556?alt=media&token=3a90790e-7e52-4628-954a-ef44314320c3",
+    details: "Silverware",
+    createdAt: { $date: "2022-04-13T03:48:57.673Z" },
+    updatedAt: { $date: "2022-04-13T03:48:57.673Z" },
+    __v: 0,
+  },
+  {
+    _id: "625649b4e2717fb7a93bbd15",
+    username: "Test_User",
+    title: "Face Plant Pot",
+    price: 15,
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/yeolmu-api.appspot.com/o/items%2FTest_User_item_1649822130059?alt=media&token=7e0430c5-2402-4714-b502-7cc1e5149867",
+    details: "Small plant pot",
+    createdAt: { $date: "2022-04-13T03:55:32.103Z" },
+    updatedAt: { $date: "2022-04-13T03:55:32.103Z" },
+    __v: 0,
+  },
+  {
+    _id: "626943f578ea6b2966b781cd",
+    username: "test_user2",
+    title: "Pretty Vase",
+    price: 500,
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/yeolmu-api.appspot.com/o/items%2Ftest_user2_item_1651065843890?alt=media&token=fd6ddec6-8749-4f41-824d-144381ec11db",
+    details: "Pretty Ornamental Vase",
+    createdAt: { $date: "2022-04-27T13:24:05.894Z" },
+    updatedAt: { $date: "2022-04-27T13:24:05.894Z" },
+    __v: 0,
+  },
+];
+
+module.exports = items;
